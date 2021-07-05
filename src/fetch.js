beabeceed9d96ba1b8d10d276b1702b5c6040647
Amybiubiu <1246457100@qq.com>
2021-07-06 00:19:25 +0800

    init

---
+++ src/fetch.js
@@ -0,0 +1,30 @@
+const preUrl = 'http://rap2api.taobao.org/app/mock/286372'
+
+const Fetch = (url, data = {}, method = 'GET') => {
+    return fetch(preUrl+url, {
+        body: JSON.stringify(data),
+        headers: {
+            'content-type': 'application/json'
+        },
+        method: method
+    }).then(res => {
+        switch (res.statusCode) {
+            case 200:
+              if (res.data) {
+                return res.data;
+              } else {
+                return res.code; // 业务逻辑错误，返回业务错误码
+              }
+            case 400:
+              throw new Error('没有权限访问');
+            case 401:
+              throw new Error('未授权');
+            case 404:
+              throw new Error('not found');
+            case 500:
+              throw new Error('服务器错误');
+            default:
+              throw new Error('未知错误');
+          }
+    })
+}
