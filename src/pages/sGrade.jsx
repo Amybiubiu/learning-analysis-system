e8818f986d4bbafacef7d3cf02dbc31e129444af
Charlotte1119 <85163245+Charlotte1119@users.noreply.github.com>
2021-07-08 13:53:38 +0800

    Update sGrade.jsx

--- src/pages/sGrade.jsx
+++ src/pages/sGrade.jsx
@@ -1,7 +1,62 @@
+
 import React from 'react'
 
-const SGrade = () => {
-    return <div>sgrade</div>
+import { Table } from 'antd'
+
+
+class SGrade extends React.Component{
+    render()
+    {
+       const columns1=[
+            {
+                title:' ',
+                dataIndex:'name',
+                width:100,
+            },
+            {
+                title:'软件工程导论',
+                dataIndex:'grade1',
+                width:300,
+                
+             },
+            {
+                title:'人工智能',
+                dataIndex:'grade2',
+                width:300,
+
+            },
+            {
+                title:'编译原理',
+                dataIndex:'grade3',
+                width:300,
+
+                
+             }]
+
+            const data1 = [
+                {
+                    name:'张三',
+                    grade1:'98',
+                    grade2:'54',
+                    grade3:'70'
+        
+                }]
+    
+           
+            return (
+                <div>             
+                   
+                    <Table
+                    columns={columns1}
+                    dataSource={data1}
+                    pagination={false}
+                    />
+                
+                </div>
+                )
+    }
+    
 }
+
 
 export default SGrade
