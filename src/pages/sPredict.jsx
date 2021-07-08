cbb4da29f05d2c0b04dde4fb8bf7169e2ba92a6c
Charlotte1119 <85163245+Charlotte1119@users.noreply.github.com>
2021-07-08 13:55:11 +0800

    Update sPredict.jsx

--- src/pages/sPredict.jsx
+++ src/pages/sPredict.jsx
@@ -1,7 +1,10 @@
 import React from 'react'
 
 const SPredict = () => {
-    return <div>spredict</div>
+    return <div><br/>你的《软件工程导论》成绩非常不错。<br/><br/><br/>
+    <strong>你的《人工智能》成绩不及格！</strong><br/><br/><br/>
+    你的《编译原理》成绩一般，请继续努力。</div>
+
 }
 
 export default SPredict
